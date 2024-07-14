5960fa9169e754e647ae281e9ace6ea8d4c9c25f
Abdullah Emad <amenesy8@gmail.com>
2024-07-14 21:42:01 +0300

    This commit for separated branch as a demo for viewing YT video in web and mobile

--- lib/app/onBoarding/view/pages/on_boarding.dart
+++ lib/app/onBoarding/view/pages/on_boarding.dart
@@ -1,14 +1,51 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:tut_app/core/theme/colors_manager.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
-class OnBoardingPage extends StatelessWidget {
+class OnBoardingPage extends StatefulWidget {
   static const String routeName = '/on_boarding';
   const OnBoardingPage({super.key});
+
+  @override
+  State<OnBoardingPage> createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      params: const YoutubePlayerParams(
+        showControls: true,
+        mute: false,
+        showFullscreenButton: true,
+        loop: false,
+      ),
+    );
+
+    _controller.setFullScreenListener(
+      (isFullScreen) {
+        log('${isFullScreen ? 'Entered' : 'Exited'} Fullscreen.');
+      },
+    );
+
+    _controller.cueVideoById(videoId: 'U_32rq7gj7E');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary,
+      body: Center(
+        child: YoutubePlayer(
+          controller: _controller,
+          aspectRatio: 16 / 9,
+        ),
+      ),
     );
   }
 }
