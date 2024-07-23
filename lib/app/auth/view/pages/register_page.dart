2fa80a1bf8748cb379d34d74bdd056cf9d0c5cef
Abdullah Emad <amenesy8@gmail.com>
2024-07-24 00:59:35 +0300

    Implementing Register Page UI

--- lib/app/auth/view/pages/register_page.dart
+++ lib/app/auth/view/pages/register_page.dart
@@ -1,9 +1,14 @@
+// ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tut_app/app/auth/view/widgets/auth_title.dart';
 import 'package:tut_app/app/auth/view/widgets/input_field.dart';
+import 'package:tut_app/app/auth/view/widgets/profile_pic_avatar.dart';
+import 'package:tut_app/app/auth/view/widgets/register_button.dart';
+import 'package:tut_app/app/auth/view_model/register/register_cubit.dart';
 import 'package:tut_app/core/app_strings.dart';
-import 'package:tut_app/core/assets_paths.dart';
-import 'package:tut_app/core/theme/colors_manager.dart';
 import 'package:tut_app/core/values_manager.dart';
 
 class RegisterPage extends StatelessWidget {
@@ -12,35 +17,91 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final registerCubit = context.read<RegisterCubit>();
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 30,
-              ),
-              const AuthTitle(title: AppStrings.register),
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                height: AppSize.s130,
-                width: AppSize.s130,
-                child: CircleAvatar(
-                  backgroundColor: AppColors.primary,
-                  backgroundImage: const AssetImage(ImagesAssets.avatar),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              InputFieldWidget(labelText: AppStrings.name)
-            ],
+          child: BlocBuilder<RegisterCubit, RegisterState>(
+            builder: (context, state) {
+              XFile? profilePic;
+              if (state is UploadImageSuccess) {
+                profilePic = state.image;
+              }
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: AppSize.s30,
+                  ),
+                  const AuthTitle(title: AppStrings.register),
+                  const SizedBox(
+                    height: AppSize.s4,
+                  ),
+                  Center(child: ProfilePicAvatar(profilePic: profilePic)),
+                  const SizedBox(height: AppSize.s12),
+                  InputFieldWidget(
+                    labelText: AppStrings.name,
+                    hintText: AppStrings.yourNameEx,
+                    controller: registerCubit.nameController,
+                    validator: _handleValidation,
+                  ),
+                  const SizedBox(height: AppSize.s18),
+                  InputFieldWidget(
+                    labelText: AppStrings.email,
+                    hintText: AppStrings.emailHint,
+                    controller: registerCubit.emailController,
+                    validator: _handleValidation,
+                  ),
+                  const SizedBox(height: AppSize.s18),
+                  InputFieldWidget(
+                    labelText: AppStrings.phoneNumber,
+                    hintText: AppStrings.phoneNumberHint,
+                    controller: registerCubit.phoneController,
+                    validator: _handleValidation,
+                  ),
+                  const SizedBox(height: AppSize.s18),
+                  InputFieldWidget(
+                    labelText: AppStrings.password,
+                    hintText: AppStrings.passwordHint,
+                    controller: registerCubit.passwordController,
+                    passwordInput: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty)
+                        return AppStrings.emptyFields;
+                      if (value.length < 8) return AppStrings.passwordHint;
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: AppSize.s18),
+                  InputFieldWidget(
+                    labelText: AppStrings.confirmPassword,
+                    hintText: AppStrings.confirmPasswordHint,
+                    controller: registerCubit.confirmPasswordController,
+                    passwordInput: true,
+                    validator: (p0) {
+                      if (p0 == null || p0.isEmpty)
+                        return AppStrings.emptyFields;
+                      if (p0 != registerCubit.passwordController.text)
+                        return AppStrings.passwordNotMatch;
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: AppSize.s18),
+                  RegisterButton(text: AppStrings.register, onTap: () {})
+                ],
+              );
+            },
           ),
         ),
       ),
     );
+  }
+
+  String? _handleValidation(String? value) {
+    if (value == null || value.isEmpty) {
+      return AppStrings.emptyFields;
+    }
+    return null;
   }
 }
